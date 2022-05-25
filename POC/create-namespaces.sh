d468beb04a875600c087a1d4c4e762c04969da32
Alexandre Alvino <alexandre.aaa@hotmail.com>
2022-05-24 23:24:49 -0300

    Rollback create namespaces script

--- POC/create-namespaces.sh
+++ POC/create-namespaces.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 kubectl create ns istio-system
-kubectl apply -f configmaps.yaml
-
-(cd spire ; ./deploy-spire.sh)
-(cd istio ; ./deploy-istio.sh)
-(cd bookinfo ; ./deploy-bookinfo.sh)
+kubectl create ns spire
